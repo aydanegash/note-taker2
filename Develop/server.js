5a933ce34da52eb978cd525cc25579b8e8036281
aydanegash <73369360+aydanegash@users.noreply.github.com>
2021-07-09 19:08:51 -0500

    updated server.js file and added routes folder with apiRoutes and htmlRoutes

--- Develop/server.js
+++ Develop/server.js
@@ -1,19 +1,22 @@
-
 const express = require('express');
 const app = express();
+const path = require('path');
 
+const fs = require('fs');
+const util = require('fs');
 
-const PORT = process.env.PORT || 8000;
+const uuid = require("uuid-random");
 
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
-// require('./routes/apiRoutes')(app);
-// require('./routes/htmlRoutes')(app);
-
-
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-  console.log(`App listening on PORT: https:${PORT}`);
+  console.log(`App listening on PORT: http://localhost:${PORT}`);
 });
